d7a9d0f245d5425ab6f4a70f589e3b034a2d29eb
Bogdan M <bogdancons99@gmail.com>
2024-06-06 15:11:32 +0300

    add challenges

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,19 @@
+const TimerChallenge = ({title, targetTime}) => {
+    return <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+            {targetTime} second{targetTime>1?'s':''}
+        </p>
+        <p>
+
+        <button>
+            StartChallenge
+        </button>
+        </p>
+        <p className="">
+            Time is running
+        </p>
+    </section>
+}
+
+export default TimerChallenge;
